3030af533725c1e36e620661e623e0f3c460d251
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-11-19 22:13:11 +0300

    end001

--- HomeWork002/obj/Debug/net6.0/HomeWork002.AssemblyInfo.cs
+++ HomeWork002/obj/Debug/net6.0/HomeWork002.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("homework001")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork002")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("homework001")]
-[assembly: System.Reflection.AssemblyTitleAttribute("homework001")]
+[assembly: System.Reflection.AssemblyProductAttribute("HomeWork002")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HomeWork002")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
