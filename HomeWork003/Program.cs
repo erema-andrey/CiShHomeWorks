4c3c46442508e867ae6712c5cd4594cf8980fe5c
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-11-23 12:22:27 +0300

    begin

---
+++ HomeWork003/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("введите число A");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int level = num;
+// for (int i = 1; i < num1; i++)
+// {
+// level = level * num;
+// }
+// Console.WriteLine("A в степени B равно: " + level);
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// int GetSum(int num)
+// {
+//     int sum = 0;
+//     for(int i = 0; i <= num; i++)
+//     {
+//         sum += i;
+        
+//     }
+//     return  sum;
+// }
+
+
+
+// Console.WriteLine("введите число");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (i > 0)
+// {
+// int num = i % 10;
+// i = i / 10;
+// sum = sum + num;
+// }
+// Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
+
+
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33
--- HomeWork003/obj/Debug/net6.0/HomeWork003.AssemblyInfo.cs
+++ HomeWork003/obj/Debug/net6.0/HomeWork003.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork002")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HomeWork002")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HomeWork002")]
+[assembly: System.Reflection.AssemblyProductAttribute("HomeWork003")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HomeWork003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
