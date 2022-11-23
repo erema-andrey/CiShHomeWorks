34d8ea07d6af1d1e3a1ba967039a46b012785539
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-11-23 17:42:09 +0300

    end

--- HomeWork003/Program.cs
+++ HomeWork003/Program.cs
@@ -18,29 +18,19 @@
 
 
 
+
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
 
 
-// int GetSum(int num)
-// {
-//     int sum = 0;
-//     for(int i = 0; i <= num; i++)
-//     {
-//         sum += i;
-        
-//     }
-//     return  sum;
-// }
 
 
 
 // Console.WriteLine("введите число");
 // int i = Convert.ToInt32(Console.ReadLine());
 // int sum = 0;
-
 // while (i > 0)
 // {
 // int num = i % 10;
@@ -59,58 +49,40 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33
 
-int[] array = new int[8];
-PrintArray(array);
-
-Console.WriteLine();
-var arr = FillArray(8);
-
-PrintArray(arr);
 
 
-int[] FillArray(int length)
+// int[] array = new int[8];
+// PrintArray(array);
+// Console.WriteLine();
+// var arr = FillArray(8);
+// PrintArray(arr);
+// int[] FillArray(int length)
+// {
+// int[] result = new int[length];
+// for(int i =0; i < result.Length; i++)
+// {
+//      result[i] = new Random().Next(0, 11);
+// }
+// return result;
+// }
+// void PrintArray(int[] arr)
+// {
+// for(int i = 0; i < arr.Length; i++)
+// {
+//  Console.Write($"{arr[i]}, ");
+//  }
+// }
 
-{
-   
-int[] result = new int[length];
-   
-for(int i =0; i < result.Length; i++)
-    
-{
-        
-      result[i] = new Random().Next(0, 2);
-    
-}
-    
-return result;
 
-}
-
-
-void PrintArray(int[] arr)
-
-{
-    
-for(int i = 0; i < arr.Length; i++)
-    
-{
-       
- Console.Write($"{arr[i]}, ");
-   
- }
-
-}
 
 // int [] numbers = new int[8];
 // Console.Write("[");
-
 // for (int i = 0; i < numbers.Length; i++)
 //  {
 //     numbers [i] = new Random().Next(0, 11);
 //     Console.Write(" " + Method (i) + " ");
 //  }
 // Console.Write("]");
-
 // int Method (int a)
 // {
 //     return numbers[a];
