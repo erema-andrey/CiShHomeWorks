908ced0a380a62bf97327fd5a9d3c7f2305cc310
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-11-28 22:55:51 +0300

    end004

--- HomeWork003/Program.cs
+++ HomeWork003/Program.cs
@@ -44,14 +44,17 @@
 // Console.WriteLine($"Количество четных чисел в массиве ----> {evenNumber}");
 
 
+
+
 // Console.Write("Введите элементы массива:\t");
 // int number = Convert.ToInt32(Console.ReadLine());
 // int[] arry = new int[number];
 // FillArray(array); 
 // PrintArray(array);
+// Console.WriteLine();
 // Console.WriteLine($"{"Количество четных чисел:\t",33}{EvenNum(array)}");
 
-// int EvenNum(int[]arr)
+// int EvenNum(int[] arr)
 // {
 //      int amounts = 0;
 //      foreach (int item in arr)
@@ -63,7 +66,6 @@
 //      }
 //      return amounts;
 // }   
-                
 // void FillArray(int[] arr)
 // {
    
@@ -86,7 +88,6 @@
 //             Console.Write($"{arr[i]}, ");
 //         }
 //     }
-
 // }
 
 
@@ -96,17 +97,18 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
- 
+// Console.Write("Введите элементы массива:\t");
+// int number = Convert.ToInt32(Console.ReadLine());
 
+// int[] arry = new int[number];
+// FillArray(array); 
+// PrintArray(array);
+// Console.WriteLine();
+// Console.WriteLine($"Сумма нечетных индексов = {OddSum(array)}");
 
-// var array = FillArray(4);
-// Print (array);
-// Console.WriteLine(); 
-// EvenNum(array);
-// Print(array);
-
-
-//  int summ = 0;
+// int OddSum(int[]arr)
+// {
+//             int summ = 0;
 //             for (int i = 0; i < 0; i++)
 //             {
 //                 for (int j = 0; j < 0; j++)
@@ -114,15 +116,15 @@
 //                 if (array[i, j]%2 != 0) summ += array[i, j];
 //                 }
 //             }
-//             Console.WriteLine($"Сумма нечётных элементов массива: {summ}");
-
+//             return sum;
+// }
 
 // int[] FillArray(int length)
 // {
 //     int[] array = new int[length];
 //     for(int i = 0; i < length; i++)
 //     {
-//         array[i] = new Random().Next(100,999);
+//         array[i] = new Random().Next(1,99);
 //     }
 //     return array;
 // }
@@ -140,19 +142,4 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-// int[] FillArray(int length)
-// {
-//     int[] array = new int[length];
-//     for(int i = 0; i < length; i++)
-//     {
-//         array[i] = new Random().Next(100,999);
-//     }
-//     return array;
-// }
-// void Priht(int[] array)
-// {
-//      foreach(var item in array)
-//      {
-//         Console.WriteLine($"{item}, ");
-//      }
-// }
+
