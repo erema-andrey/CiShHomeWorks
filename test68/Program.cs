d9c5def901b8c9608f398d171ed8170421df625f
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-12-10 13:36:14 +0300

    endfinish

--- test68/Program.cs
+++ test68/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿    // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+    //  Даны два неотрицательных числа m и n.
+	
+		
+		int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(3,2));
