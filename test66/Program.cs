fec55df221e17886a69a8baefd00f70740df109c
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-12-10 13:44:09 +0300

    endFinish

--- test66/Program.cs
+++ test66/Program.cs
@@ -1,22 +1,18 @@
-﻿		// Задача 66: Задайте значения M и N. Напишите программу,
-        //  которая найдёт сумму натуральных элементов в промежутке от M до N.
-		// M = 1; N = 15 -> 120
-		// M = 4; N = 8. -> 30
-		
-		// Задача 66: Задайте значения M и N. Напишите программу, 
-        // которая найдёт сумму натуральных элементов в промежутке от M до N.
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 		
 		
-		int CalculateSumm(int m, int n)
-		{
-			int start = m;
-			int end = n;
-			if(m > n)
-			{
-				start = n;
-				end = m;
-			}
-			return (end + start)*(end - start + 1)/2;		}
+int CalculateSumm(int m, int n)
+{
+	int start = m;
+	int end = n;
+	if(m > n)
+	{
+		start = n;
+		end = m;
+	}
+	return (end + start)*(end - start + 1)/2;		
+}
 		
-		Console.WriteLine(CalculateSumm(72, 16));
+Console.WriteLine(CalculateSumm(72, 16));
 	
