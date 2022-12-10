8bad432deecd9548ab5375b9c9536ad3ef0e136b
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2022-12-10 13:20:45 +0300

    end001

---
+++ test64/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+System.Console.WriteLine(PrintNumbers(0, 10));
+
+string PrintNumbers(int start, int end)
+
+{
+
+    if (start == end)
+
+    {
+
+        return start.ToString();
+
+    }
+    return (start + " " + PrintNumbers(++start, end));
+
+}
+
